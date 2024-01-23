571c5b832fa8d0447e76874c1ae5389adb63c9b8
Yahya Faheem <yahya.faheem@trax.pk>
2024-01-23 17:26:20 +0500

    search functionality in about

---
+++ src/components/EmployeeCard.vue
@@ -0,0 +1,18 @@
+<template>
+    <ul>
+      <li v-for="emp in filteredEmployees" :key="emp">
+        {{ emp }}
+      </li>
+    </ul>
+</template>
+
+<script lang="ts">
+import { textSpanIntersectsWith } from 'typescript';
+
+
+export default {
+    name: 'EmployeeCard',
+
+};
+
+</script>
--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,8 +1,32 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <input type="search" placeholder="search employees" v-model="searchVal" @input="searchEmployees">
+    <EmployeeCard />
   </div>
 </template>
+
+
+<script lang="ts" setup>
+import { ref } from 'vue';
+import { EmployeeCard } from './EmployeeCard.vue';
+
+let searchVal = ref('');
+
+let employees = [
+  'John Doe',
+  'Jane Doe',
+  'Jenny Lane',
+  'Roberto Lewis',
+  'James Neil'
+];
+
+let filteredEmployees = ref(employees);
+
+let searchEmployees = () => {
+  filteredEmployees.value = employees.filter((emp) => emp.toLowerCase().includes(searchVal.value.toLowerCase()));
+};
+
+</script>
 
 <style>
 @media (min-width: 1024px) {
